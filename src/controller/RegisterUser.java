e7fa86453d7cae519771e38669ebb0b242948efc
DieterDraelants <dieter.draelants@student.ucll.be>
2020-05-08 01:08:29 +0200

    person is registerable

--- src/controller/RegisterUser.java
+++ src/controller/RegisterUser.java
@@ -1,5 +1,8 @@
 package controller;
 
+import domain.Person;
+import domain.Role;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,7 +18,18 @@ public class RegisterUser extends RequestHandler {
         String password = request.getParameter("password");
         String repassword = request.getParameter("repassword");
 
-        System.out.println(userId + firstname + lastname + gender + password + repassword);
+        if(password.equals(repassword)) {
+            Person p = new Person(userId, password, firstname,lastname, Role.LID);
+            p.setGender(gender);
+            getPersonService().addPerson(p);
+        } else {
+            throw new IllegalArgumentException("Password not okey buddy, not matching");
+        }
+
+
+        //test if the user is added to the list
+        System.out.println(getPersonService().getPersons());
+
         response.sendRedirect("Controller?action=ShowHome");
     }
 }
